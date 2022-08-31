4aae8f0abb746c2e44a8dfd218f2f53f83ae82da
good-day12 <106358554+good-day12@users.noreply.github.com>
2022-08-31 12:01:30 -0700

    Operator Subclasses
    Added the Operator subclasses and updated their priorities and execute methods

---
+++ calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
@@ -0,0 +1,21 @@
+package edu.csc413.calculator.operators;
+
+import edu.csc413.calculator.evaluator.Operand;
+
+public class PowerOperator extends Operator {
+    @Override
+    public int priority() {
+        return 3;
+    }
+
+    @Override
+    public Operand execute(Operand operandOne, Operand operandTwo) {
+        //to hold our result value
+        int result = operandOne.getValue();
+        //for loop to multiply operandOne value by itself for operandTwo number of times
+        for (int i = 0; i < operandTwo.getValue(); i++){
+            result *= operandOne.getValue();
+        }
+        return new Operand(result);
+    }
+}
